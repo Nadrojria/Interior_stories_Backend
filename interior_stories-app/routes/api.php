7f3357a32409547d3ddfcb7112fb3f6ccac16eac
Nadrojria <loicdesbordes@yahoo.fr>
2025-07-02 12:14:27 +0200

    add: login auth and start logout

--- interior_stories-app/routes/api.php
+++ interior_stories-app/routes/api.php
@@ -2,7 +2,39 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 Route::get('/users', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::post('/login', function (Request $request) {
+    if (auth()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+        $user = auth()->user();
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+    return response()->json([
+        'user'          => $user,
+        'access_token'  => $token,
+        'token_type'    => 'Bearer'
+    ]);
+    }
+    
+    return response()->json([
+        'error' => 'Unauthenticated user',
+        'code' => 401,
+    ], 401);
+});
+
+
+Route::post('/logout', function (Request $request) {
+    $user->tokens()->where('id', $tokenId)->delete();
+    Auth::logout();
+    return response()->json([
+      'message' => 'Logout successfull'
+    ]);
+})->middleware('auth:sanctum');
+
+
