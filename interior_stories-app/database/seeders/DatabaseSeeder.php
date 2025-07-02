c00bad3d8380647257e5421676feabdd3600abc2
Nadrojria <loicdesbordes@yahoo.fr>
2025-07-02 10:57:53 +0200

    add: seeder user for add new user

--- interior_stories-app/database/seeders/DatabaseSeeder.php
+++ interior_stories-app/database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test User 2',
             'email' => 'test@example.com',
+            'password' => Hash::make('test')
         ]);
     }
 }
